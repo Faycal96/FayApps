0356ae6b75668ca3618d20198dd60686a24a38c9
Issouf <issoufouedraogo924@gmail.com>
2024-02-03 11:24:41 +0000

    cc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <p>
+            @if(session('error'))
+<div class="alert alert-danger">
+{{ session('error') }}
+</div>
+@endif
+        </p>
         <div class="col-md-8">
             <div class="card">
+
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
